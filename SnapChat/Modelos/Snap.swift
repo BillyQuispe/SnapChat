76cef093e7b29ce6cd3b5494b7d65a0354e66e7e
Billy <carlos.quispe.v@tecsup.edu.pe>
2024-06-03 15:01:06 -0500

    Lab13 terminado

---
+++ SnapChat/Modelos/Snap.swift
@@ -0,0 +1,16 @@
+//
+//  Snap.swift
+//  SnapChat
+//
+//  Created by Carlos Velasquez on 3/06/24.
+//
+
+import Foundation
+
+class Snap{
+    var imagenURL = ""
+    var descrip = ""
+    var from = ""
+    var id = ""
+    var imagenID = ""
+}
